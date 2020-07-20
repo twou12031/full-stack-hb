674ebeea2be25e746121ca2ac925cfcaa0651506
haobai <twou12031@163.com>
2020-07-20 15:07:56 +0800

    lesson Course with add new and filter

--- src/App.js
+++ src/App.js
@@ -1,64 +1,56 @@
-import React from 'react'
-import Course from './components/Course/Course'
+import React, { useState } from 'react'
+import Note from './components/Note'
 
-const App = () => {
-    const courses = [
-        {
-          name: 'Half Stack application development',
-          id: 1,
-          parts: [
-            {
-              name: 'Fundamentals of React',
-              exercises: 10,
-              id: 1
-            },
-            {
-              name: 'Using props to pass data',
-              exercises: 7,
-              id: 2
-            },
-            {
-              name: 'State of a component',
-              exercises: 14,
-              id: 3
-            },
-            {
-              name: 'Redux',
-              exercises: 11,
-              id: 4
-            }
-          ]
-        },
-        {
-          name: 'Node.js',
-          id: 2,
-          parts: [
-            {
-              name: 'Routing',
-              exercises: 3,
-              id: 1
-            },
-            {
-              name: 'Middlewares',
-              exercises: 7,
-              id: 2
-            }
-          ]
+const App = (props) => {
+    const [notes, setNotes] = useState(props.notes)
+
+    const [newNote, setNewNote] = useState('')
+
+    const [showAll, setShowAll] = useState(true)
+
+    const inputChangeHandler = event => {
+        console.log('@input change')
+        console.log(event.target.value)
+        setNewNote(event.target.value)
+    }
+
+    const addNote = event => {
+        event.preventDefault()
+        const newNoteData = {
+            id: notes.length + 1,
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random < 0.5
         }
-      ]
+        setNotes(notes.concat(newNoteData))
+        setNewNote('')
+    }
 
+    const notesToShow = showAll
+        ? notes
+        : notes.filter(e => e.important)
 
-
-      return (
+    return (
         <div>
-            {
-                courses.map(course => {
-                    return <Course key={course.id} course={course} />
-                })
-            }
+            <h1>Notes</h1>
+            <div>
+                <button onClick={()=>{
+                    setShowAll(!showAll)
+                }}>
+                    查看{showAll ? '重要': '全部'}
+                </button>
+            </div>
+            <ul>
+                {notesToShow.map(note =>
+                    <Note key={note.id} note={note} />
+                )}
+            </ul>
+            <form onSubmit={addNote}>
+                <input placeholder="a new note..." value={newNote} onChange={inputChangeHandler}/>
+                <button type="submit">save</button>
+            </form>
         </div>
-      )
-
-  }
+    )
+}
 
 export default App
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Note = ({note}) => (
-    <li>{note}</li>
+    <li>{note.content}</li>
 )
 
 export default Note
