ca5c6007541d4c68a97b341421b2da499c801f74
haobai <twou12031@163.com>
2020-07-21 14:43:23 +0800

    lesson AJAX

--- src/App.js
+++ src/App.js
@@ -1,87 +1,36 @@
-import React, { useState } from 'react'
-import Person from './components/PhoneBook/Person'
-import PersonForm from './components/PhoneBook/PersonForm'
-import Filter from './components/PhoneBook/Filter'
+import React, { useState, useEffect } from 'react'
+import Note from './components/Note'
+
+import axios from 'axios'
+
+
 
 const App = () => {
-    const [ persons, setPersons ] = useState([
-            {
-                id: 1,
-                name: 'Arto Hellas',
-                phone: '13133333333'
-            },
-            {
-                id: 2,
-                name: 'Ada Lovelace',
-                phone: '39-44-5323523'
-            },
-            {
-                id: 3,
-                name: 'Dan Abramov',
-                phone: '12-43-234345' },
-            {
-                id: 4,
-                name: 'Mary Poppendieck',
-                phone: '39-23-6423122'
-            }
-        ])
-    const [ newName, setNewName ] = useState('')
-    const [ newPhone, setNewPhone ] = useState('')
+    const [notes, setNotes] = useState([])
 
-    const [ filterPhone, setFilterPhone ] = useState('')
+    useEffect(() => {
+        console.log('@effect')
 
-    const submitHandler = ev => {
-        ev.preventDefault()
+        axios
+            .get('http://localhost:3001/notes')
+            .then(res => {
+                setNotes(res.data)
+                console.log(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
 
-        const hasSame = persons.findIndex( e => {
-            return e.name === newName
-        }) !== -1
+    }, [])
 
-        if (hasSame) {
-            alert(`${newName} has already added to the phonebook`)
-            return
-        }
-
-        setPersons(persons.concat({
-            id: persons.length + 1,
-            name: newName,
-            phone: newPhone
-        }))
-        setNewName('')
-        setNewPhone('')
-    }
-
-
-    const needShow = filterPhone.length <= 0
-        ? persons
-        : persons.filter(e => {
-            return e.phone.match(filterPhone)
-        })
-
-    const filterHandler = val => {
-        console.log(val)
-        setFilterPhone(val)
-    }
+    console.log('render', notes.length, 'notes')
 
     return (
         <div>
-            <h2>Phonebook</h2>
-            <PersonForm newName={newName}
-                newPhone={newPhone}
-                submitHandler={submitHandler}
-                setNewName={(val) => {
-                    setNewName(val)
-                }}
-                setNewPhone={(val) => {
-                    setNewPhone(val)
-                }}></PersonForm>
-            <h2>Filter</h2>
-            <Filter filterHandler={filterHandler}></Filter>
-            <h2>Numbers</h2>
             {
-                needShow.map( e => {
+                notes.map( e => {
                     return (
-                        <Person person={e} key={e.id}></Person>
+                        <Note note={e} key={e.id}></Note>
                     )
                 })
             }
