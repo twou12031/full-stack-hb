cf0bcd7b7f3d1f70e38985984f034a0bce388f08
haobai <twou12031@163.com>
2020-07-21 14:49:47 +0800

    lesson AJAX homework one

--- src/App.js
+++ src/App.js
@@ -1,36 +1,81 @@
 import React, { useState, useEffect } from 'react'
-import Note from './components/Note'
+import PersonForm from './components/PhoneBook/PersonForm'
+import Filter from './components/PhoneBook/Filter'
+import Person from './components/PhoneBook/Person'
 
 import axios from 'axios'
 
 
 
+
 const App = () => {
-    const [notes, setNotes] = useState([])
+    const [ persons, setPersons ] = useState([])
+    const [ newName, setNewName ] = useState('')
+    const [ newPhone, setNewPhone ] = useState('')
+
+    const [ filterPhone, setFilterPhone ] = useState('')
 
     useEffect(() => {
-        console.log('@effect')
-
         axios
-            .get('http://localhost:3001/notes')
+            .get('http://localhost:3001/persons')
             .then(res => {
-                setNotes(res.data)
-                console.log(res.data)
+                const { data } = res
+                setPersons(data)
             })
-            .catch(err => {
-                console.log(err)
-            })
-
     }, [])
 
-    console.log('render', notes.length, 'notes')
+    const submitHandler = ev => {
+        ev.preventDefault()
+
+        const hasSame = persons.findIndex( e => {
+            return e.name === newName
+        }) !== -1
+
+        if (hasSame) {
+            alert(`${newName} has already added to the phonebook`)
+            return
+        }
+
+        setPersons(persons.concat({
+            id: persons.length + 1,
+            name: newName,
+            phone: newPhone
+        }))
+        setNewName('')
+        setNewPhone('')
+    }
+
+
+    const needShow = filterPhone.length <= 0
+        ? persons
+        : persons.filter(e => {
+            return e.phone.match(filterPhone)
+        })
+
+    const filterHandler = val => {
+        console.log(val)
+        setFilterPhone(val)
+    }
 
     return (
         <div>
+            <h2>Phonebook</h2>
+            <PersonForm newName={newName}
+                newPhone={newPhone}
+                submitHandler={submitHandler}
+                setNewName={(val) => {
+                    setNewName(val)
+                }}
+                setNewPhone={(val) => {
+                    setNewPhone(val)
+                }}></PersonForm>
+            <h2>Filter</h2>
+            <Filter filterHandler={filterHandler}></Filter>
+            <h2>Numbers</h2>
             {
-                notes.map( e => {
+                needShow.map( e => {
                     return (
-                        <Note note={e} key={e.id}></Note>
+                        <Person person={e} key={e.id}></Person>
                     )
                 })
             }
